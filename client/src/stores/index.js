7b246fcd9e76bb095823c235d1fb0ae4a231255e
Steve Roche <1steveroche@gmail.com>
2019-03-02 20:23:14 +0530

    Typescript

--- client/src/stores/index.js
+++
@@ -1,5 +0,0 @@
-import { observable } from 'mobx';
-
-class User {
-	@observable
-}
---
+++ client/src/stores/index.ts
@@ -0,0 +1,11 @@
+import { observable } from 'mobx';
+
+class User {
+	@observable username = '';
+	@observable email = '';
+	@observable region1 = '';
+	@observable region2 = '';
+	@observable region3 = '';
+	@observable region4 = '';
+	@observable region5 = '';
+}
