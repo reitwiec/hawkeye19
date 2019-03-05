e43fb5d49ccc260aa1477a2a4b68511c0d0530e2
reitwiec <reitwiec@gmail.com>
2019-03-05 10:13:39 +0530

    support page and question page skeleton added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,39 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 
-import { Dashboard, LoginPage, QuestionPage, RegisterPage } from './pages';
-import { AdminRouter } from './routers';
+import {
+  Dashboard,
+  LoginPage,
+  QuestionPage,
+  RegisterPage,
+  SupportPage
+} from "./pages";
+import { AdminRouter } from "./routers";
 
 class App extends Component {
-	render() {
-		return (
-			<div>
-				<CookiesProvider>
-					<Router>
-						<Switch>
-							<Route exact path="/" render={() => <LoginPage />} />
-							<Route exact path="/register" render={() => <RegisterPage />} />
-							<Route exact path="/dashboard" render={() => <Dashboard />} />
-							<Route
-								exact
-								path="/question"
-								render={props => <QuestionPage {...props} />}
-							/>
-							<Route path="/___admin" component={AdminRouter} />
-						</Switch>
-					</Router>
-				</CookiesProvider>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <CookiesProvider>
+          <Router>
+            <Switch>
+              <Route exact path="/" render={() => <LoginPage />} />
+              <Route exact path="/register" render={() => <RegisterPage />} />
+              <Route exact path="/dashboard" render={() => <Dashboard />} />
+              <Route
+                exact
+                path="/question"
+                render={props => <QuestionPage {...props} />}
+              />
+              <Route path="/___admin" component={AdminRouter} />
+              <Route exact path="/support" render={() => <SupportPage />} />
+            </Switch>
+          </Router>
+        </CookiesProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,5 @@
-export { default as Dashboard } from './Dashboard';
-export { default as LoginPage } from './LoginPage';
-export { default as QuestionPage } from './QuestionPage';
-export { default as RegisterPage } from './RegisterPage';
+export { default as Dashboard } from "./Dashboard";
+export { default as LoginPage } from "./LoginPage";
+export { default as QuestionPage } from "./QuestionPage";
+export { default as RegisterPage } from "./RegisterPage";
+export { default as SupportPage } from "./SupportPage";
