2a35fb862db49585d8694ff0f6ef10f9c69c6f02
Soumik Majumder <soumik712@gmail.com>
2019-03-10 17:48:18 +0530

    Removed comented code

--- api/app/models.go
+++ api/app/models.go
@@ -28,17 +28,6 @@ type Response struct {
 type CurrUser struct {
 	ID       int    `json:"id"`
 	Username string `json:"username"`
-	/*
-		Email    string `json:"email"`
-		Access   int    `json:"access"`
-		Region1  int    `json:"region1"`
-		Region2  int    `json:"region2"`
-		Region3  int    `json:"region3"`
-		Region4  int    `json:"region4"`
-		Region5  int    `json:"region5"`
-		Linear   int	`json:"linear"`
-		Points   int    `json:"points"`
-	*/
 }
 
 type App struct {
